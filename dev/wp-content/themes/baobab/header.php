9cf936776c3163937f1118f79d427e0cff37f12f
Jimmylet <jimmy.letecheur@student.hepl.be>
2016-04-28 12:08:58 +0200

    Revert "Revert "gitignore""
    This reverts commit 5663eaf432222fa9c98f4e1cbf0f29c0f2ebb04d.

---
+++ dev/wp-content/themes/baobab/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+        <title><?php bloginfo('name');?></title>
+        <link rel="stylesheet" href="css/style.css">
+        <!--[if lt IE 9]>
+          <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <h1 class="titre"><?php bloginfo('name');?></h1>
+        <p class="Slogan"><?php bloginfo('description'); ?></p>
+
+

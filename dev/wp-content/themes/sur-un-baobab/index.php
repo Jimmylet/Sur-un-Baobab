15f970846e14ee761867f95baa7440b25258858f
Jimmy Letecheur <jimmy@letecheur.me>
2016-09-27 15:03:59 +0200

    recode footer

--- dev/wp-content/themes/sur-un-baobab/index.php
+++ dev/wp-content/themes/sur-un-baobab/index.php
@@ -255,43 +255,38 @@ Template Name: Homepage
 <footer class="footer">
     <div class="footer__container">
         <section class="footer__section footer__help-us">
-            <div class="footer__container-section footer__container-section-1">
-                <h2 class="footer__section__title footer__help-us__title">Aidez-nous</h2>
-                <a href="<?php the_permalink('311');?>#donate" class="footer__help-us__link"><?php _e('Faites un don','b');?></a>
-                <a href="<?php the_permalink('311');?>#acheteralbum" class="footer__help-us__link"><?php _e('Achetez notre album photo','b');?></a>
-                <a href="<?php the_permalink('311');?>#diffuser" class="footer__help-us__link"><?php _e('Promouvoir nos vidéos','b');?></a>
-            </div>
+            <h2 class="footer__section__title footer__help-us__title">Aidez-nous</h2>
+            <a href="<?php the_permalink('311');?>#donate" class="footer__help-us__link"><?php _e('Faites un don','b');?></a>
+            <a href="<?php the_permalink('311');?>#acheteralbum" class="footer__help-us__link"><?php _e('Achetez notre album photo','b');?></a>
+            <a href="<?php the_permalink('311');?>#diffuser" class="footer__help-us__link"><?php _e('Promouvoir nos vidéos','b');?></a>
         </section>
-        <section class="footer__section footer__contact-us">
-            <div class="footer__container-section footer__container-section-2">
-                <h2 class="footer__section__title footer__contact-us__title"><?php _e('Nos derniers articles','b');?></h2>
+        <section class="footer__section footer__last-news">
+            <h2 class="footer__section__title footer__last-news__title"><?php _e('Nos derniers articles','b');?></h2>
 
-                <?php
-                $posts = new WP_Query( ['posts_per_page' => 3, 'post_type' => 'post'] );
-                if ( $posts->have_posts() ): while ( $posts->have_posts() ): $posts->the_post();
-                ?>
+            <?php
+            $posts = new WP_Query( ['posts_per_page' => 3, 'post_type' => 'post'] );
+            if ( $posts->have_posts() ): while ( $posts->have_posts() ): $posts->the_post();
+            ?>
 
-                <a href="<?php the_permalink();?>"
-                   class="footer__contact-us__item"
-                   title="<?php _e('Vers l’article');?> <?php echo the_title();?>"
-                >
-                  <?php echo the_title();?>
-                </a>
-                <?php wp_reset_postdata(); ?>
-                <?php endwhile; endif; ?>
-            </div>
+            <a href="<?php the_permalink();?>"
+               class="footer__last-news__item"
+               title="<?php _e('Vers l’article');?> <?php echo the_title();?>"
+            >
+              <?php echo the_title();?>
+            </a>
+            <?php wp_reset_postdata(); ?>
+            <?php endwhile; endif; ?>
 
         </section>
-        <div class="footer__section__newsletter-social-container">
-            <section class="footer__section footer__contact-us">
-                <div class="footer__container-section footer__container-section-3">
-                    <h2 class="footer__section__title footer__contact-us__title"><?php _e('Contactez-nous','b');?></h2>
-                        <a href="<?php the_permalink('142');?>"
-                           class="footer__contact-us__item footer__contact-us__item-form"><?php _e('Envoyer un e-mail','b');?></a>
-                </div>
-            </section>
-            <section class="footer__section footer__social">
-                <h2 class="footer__section__title footer__social__title">Suivez-nous </h2>
+        <section class="footer__section footer__section__contactsocial">
+            <h2 aria-level="2" class="visuallyhidden">Contactez-nous et suivez-nous</h2>
+            <div class="footer__contact">
+                <span class="footer__contact__title footer__section__title"><?php _e('Contactez-nous','b');?></span>
+                <a href="<?php the_permalink('142');?>"
+                   class="footer__contact__item"><?php _e('Envoyer un e-mail','b');?></a>
+            </div>
+            <div class="footer__social">
+                <span class="footer__social__title footer__section__title">Suivez-nous </span>
                 <a href="<?php echo $easy_options['footer_fb'];?>" class="footer__social__item footer__social__item-fb"
                    title="<?php _e('Vers','b');?> Facebook">
                     <span class="visuallyhidden">Facebook</span>
@@ -313,16 +308,18 @@ Template Name: Homepage
 
                     <span class="visuallyhidden">Youtube</span>
                 </a>
-            </section>
-        </div>
+            </div>
+        </section>
     </div>
+
     <div class="footer__copyright">
         <div class="footer__copyright__container">
-            © Sur un Baobab - Design by <a href="http://letecheur.me"
+            © Sur un Baobab - Made with &nbsp;♥&nbsp;  by <a href="http://letecheur.me"
                                            title="<?php _e('Vers le site du développeur','b');?>" target="_blank">
                 Jimmy Letecheur</a>
         </div>
     </div>
+
 </footer>
 </div>
 </div>
